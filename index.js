4a10021f0b8047f6f046dbd30c480c4620139e06
SumayaMohamedSantos <smalalane3@gmail.com>
2024-03-30 19:35:32 +0200

    changes on table and using js to show and hide table

---
+++ index.js
@@ -0,0 +1,10 @@
+function showprices(){
+    var tableContent=document.getElementById("table-prices");
+
+    if(tableContent.style.display=="none"){
+        tableContent.style.display="block";
+    }else{
+        tableContent.style.display="none";
+    }
+}
+
